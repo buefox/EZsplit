085a959d6351536e53dda1f169fd75d91c43dada
buefox <bue830727@gmail.com>
2018-03-31 21:58:03 -0700

    sign in demo

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var admin = require('firebase-admin');
 
-var serviceAccount = require('../ezsplit-199720-firebase-adminsdk-t96yg-b70350bd7c.json');
-
+var serviceAccount = require('ezsplit-199720-firebase-adminsdk-t96yg-b70350bd7c.json');
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://ezsplit-199720.firebaseio.com'
